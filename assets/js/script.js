845a68f4c9676acb524c8bfad802860283c877ea
StevenM <stevenbmendoza@gmail.com>
2022-02-11 15:30:14 -0800

    js added tasks are added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form")
 var taskToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+    //prevents the browers from reloading the page (legacy behavior)
+    event.preventDefault(); 
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     taskToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
