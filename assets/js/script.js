2d13b5cf7cfbbabdc4937631d1368233e084b942
StevenM <stevenbmendoza@gmail.com>
2022-02-13 11:39:51 -0800

    format js and append taks and type to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
 var formEl = document.querySelector("#task-form")
 var taskToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
     //prevents the browers from reloading the page (legacy behavior)
     event.preventDefault(); 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item, give it a class name,
+    //add HTML content to div
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    //add HTML content to div
+    taskInfoEl.innerHTML  = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
     taskToDoEl.appendChild(listItemEl);
 };
 
